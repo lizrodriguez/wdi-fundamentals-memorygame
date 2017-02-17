1ead0d41816320b565d16dae4414568ff5c1d934
Liz Rodriguez <ecrodriguez@gmail.com>
2017-02-17 12:33:36 -0500

    changed JS code for cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,21 +1,32 @@
-console.log("JS file is connected to HTML! Woo!")
+/*console.log("JS file is connected to HTML! Woo!")
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+*/
 
+var createCards = function(){
+var gameBoard = document.getElementById("game-board");    
+
+for(var i=0; i<4; i++){
+   var div = document.createElement('div');
+   div.className="card";
+   gameBoard.appendChild(div);
+   }//why don't we need to reference [i]?
+}
+createCards();
 
 /*if ((cardOne && cardTwo === "queen") || (cardThree && cardFour === "king")){
   alert('You found a match!');
 } else {
   alert('Sorry, try again.');
-}*/
+}
 
-
+//second try for If stmt
 if (cardOne === cardTwo) {
 	alert('You found a match!');	
 }else if (cardThree === cardFour) {
 	alert('You found a match!');	
 }else {
 	alert('Sorry, try again.');
-}
+}*/
